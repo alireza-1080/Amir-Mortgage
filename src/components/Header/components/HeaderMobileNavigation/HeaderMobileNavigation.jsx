fc1d009e3ab9d1e15d238c16e4073b2b80b96163
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-09-17 20:38:49 -0700

    9/17/2024 8:38 PM

--- src/components/Header/components/HeaderMobileNavigation/HeaderMobileNavigation.jsx
+++ src/components/Header/components/HeaderMobileNavigation/HeaderMobileNavigation.jsx
@@ -1,15 +1,23 @@
 import "./HeaderMobileNavigation.css"
-import React from 'react'
-import { useSelector } from "react-redux"
+import React, { useRef } from 'react'
+import { useDispatch, useSelector } from "react-redux"
 import HeaderMobileNavigationButton from "./components/HeaderMobileNavigationButton/HeaderMobileNavigationButton"
-import HeaderRatesTerms from "../HeaderRatesTerms/HeaderRatesTerms"
+import { setIsBurgerOpen } from "../../../../redux/navigationSlicer"
 
 function HeaderMobileNavigation() {
 
   const allTabs = useSelector(state => state.navigation.allTabs)
+  const outOfMobileNavItem = useRef(null)
+  const dispatch = useDispatch();
+
+  const  clickHandler = (event) => {
+    if(event.target.className === outOfMobileNavItem.current.className){
+      dispatch(setIsBurgerOpen())
+    }
+  }
 
   return (
-    <div className="header-mobile-navigation">
+    <div className="header-mobile-navigation" onClick={() => clickHandler(event)} ref={outOfMobileNavItem}>
       <div className="header-mobile-navigation__items-container">
         {allTabs.map(tab => {
           return <HeaderMobileNavigationButton key={tab.title} {...tab} />
--- src/components/HomePageServices/components/HomePageServecesCards/components/HomePageServecesCard/HomePageServecesCard.jsx
+++ src/components/HomePageServices/components/HomePageServecesCards/components/HomePageServecesCard/HomePageServecesCard.jsx
@@ -1,10 +1,20 @@
 import "./HomePageServecesCard.css"
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function HomePageServecesCard({ title, icon, index }) {
 
+  const card = useRef(null);
+
+  useEffect(() => {
+    if(index % 2) {
+      card.current.style.setProperty("--card-position", "flex-start")
+    } else {
+      card.current.style.setProperty("--card-position", "flex-end")
+    }
+  },[])
+
   return (
-    <div className="home-page-services-card">
+    <div className="home-page-services-card" ref={card}>
       <img src={icon} alt={title} className="home-page-services-card__img" />
       <h2 className="home-page-services-card__title">{title}</h2>
     </div>
