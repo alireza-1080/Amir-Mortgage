7909a3161dd60d11f4ae963e5ec087d4619be698
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-09-18 22:13:37 -0700

    9/18/2024 10:13 PM

--- src/components/HomePageTools/components/HomePageToolsBoxMobile/HomePageToolsBoxMobile.jsx
+++ src/components/HomePageTools/components/HomePageToolsBoxMobile/HomePageToolsBoxMobile.jsx
@@ -9,44 +9,32 @@ function HomePageToolsBoxMobile() {
 
     const allTools = useSelector(state => state.tools)
     const [toolCounter, setToolCounter] = useState(0)
-    const goLeftBtn = useRef(null);
-    const goRightBtn = useRef(null)
-
-    useEffect(() => {
-        if(toolCounter === 0) {
-            goLeftBtn.current.classList.add("home-page-tools-box-mobile__arrow-right--disable");
-        } else {
-            goLeftBtn.current.classList.remove("home-page-tools-box-mobile__arrow-right--disable");
-        };
-
-        if (toolCounter === allTools.length - 1) {
-            goRightBtn.current.classList.add("home-page-tools-box-mobile__arrow-right--disable")
-        } else {
-            goRightBtn.current.classList.remove("home-page-tools-box-mobile__arrow-right--disable")
-        }
-    }, [toolCounter])
 
     const leftClickHandler = () => {
-        if (toolCounter > 0) {
+        if (toolCounter !== 0) {
             setToolCounter(toolCounter - 1)
+        } else {
+            setToolCounter(allTools.lenght - 1)
         }
     }
 
     const rightClickHandler = () => {
-        if (toolCounter < allTools.length -1) {
+        if (toolCounter !== allTools.length -1) {
             setToolCounter(toolCounter + 1)
+        } else {
+            setToolCounter(0)
         }
     }
 
     return (
         <div className="home-page-tools-box-mobile">
-            <div className="home-page-tools-box-mobile__arrow" ref={goLeftBtn} onClick={leftClickHandler}>
+            <div className="home-page-tools-box-mobile__arrow" onClick={leftClickHandler}>
                 <GoTriangleLeft />
             </div>
             <div className="home-page-tools-box-mobile__item">
                 <HomePageTool {...allTools[toolCounter]}/>
             </div>
-            <div className="home-page-tools-box-mobile__arrow" ref={goRightBtn} onClick={rightClickHandler}>
+            <div className="home-page-tools-box-mobile__arrow" onClick={rightClickHandler}>
                 <GoTriangleRight />
             </div>
         </div>
