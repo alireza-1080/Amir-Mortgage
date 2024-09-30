dd6bddb4ff2c5f59d6301efbb012b64bdb289d9b
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-09-29 21:01:22 -0700

    9/29/2024 9:01 PM

--- src/redux/aboutPageTabsStatesSlicer.js
+++ src/redux/aboutPageTabsStatesSlicer.js
@@ -4,18 +4,18 @@ const initialState = {
     allTabs: ["OUR MISSION", "AMIR ABEROUMAND", "CONTACT US"],
     selectedTab: "OUR MISSION",
     primaryMission: {
-        icon: "public/images/About-Page/missions/mission.svg",
+        icon: "./public/images/About-Page/missions/mission.svg",
         title: "Our Mission",
         details: "Get you the best possible mortgage rate.",
     },
     secondaryMissions: [
         {
-            icon: "public/images/About-Page/missions/complicated.svg",
+            icon: "./public/images/About-Page/missions/complicated.svg",
             title: "Home loans are complicated",
             details: "People have questions about home financing, and it isn't easy to find answers. We wanted to clear the confusion for homeowners and first-time buyers by providing a simple, streamlined, unbiased, one-stop shop for home loans. Whether you're looking to buy your first home, save big on your monthly payment, or shop around for quotes, we've got you covered.",
         },
         {
-            icon: "public/images/About-Page/missions/simplified.svg",
+            icon: "./public/images/About-Page/missions/simplified.svg",
             title: "So we simplified the process.",
             details: "Our platform puts homeowners at the forefront, drawing top-notch loan providers from around the country who are hungry to compete for your business. By making lenders compete, you can rest assured that you are receiving their best offer. We make comparison shopping easy by giving consumers several offers to choose from in a matter of minutes!",
         },
