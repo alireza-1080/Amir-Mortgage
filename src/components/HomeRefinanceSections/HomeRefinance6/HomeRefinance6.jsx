17d015e6cd54f2e41f3893b614ce98b584a70343
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-11-12 21:01:29 -0800

    11/12/2024 9:01 PM

---
+++ src/components/HomeRefinanceSections/HomeRefinance6/HomeRefinance6.jsx
@@ -0,0 +1,10 @@
+import "./HomeRefinance6.css"
+import React from 'react'
+
+function HomeRefinance6() {
+  return (
+    <div className="">HomeRefinance6</div>
+  )
+}
+
+export default HomeRefinance6
